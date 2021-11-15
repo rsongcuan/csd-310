0f9b98a1454aa205d056be594271d667b23656bf
Ryan Songcuan <ryansongcuan@Ryans-MacBook-Air.local>
2021-11-14 21:27:09 -0600

    Commit mongodb_test.py file for Module 5.2 assignment

--- module_5/mongodb_test.py
+++ module_5/mongodb_test.py
@@ -1,3 +1,7 @@
+# Ryan Songcuan
+# 11/14/21
+# Module 5.2 Assignment
+
 from pymongo import MongoClient
 url = "mongodb+srv://admin:admin@cluster0.uwg0x.mongodb.net/myFirstDatabase?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE"
 client = MongoClient(url)
